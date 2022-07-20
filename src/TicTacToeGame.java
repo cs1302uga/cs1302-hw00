87dbde285aac5600bfa9cc5dac013a5089a5f741
Brad Barnes <bjb211@uga.edu>
2022-07-20 14:59:51 -0400

    Added starter code

---
+++ src/TicTacToeGame.java
@@ -0,0 +1,138 @@
+/**
+ * The {@code TicTacToeGame} represents tic-tac-toe games. Each instance of
+ * this class would represent a single game of tic-tac-toe. The class includes
+ * methods for checking the state of the game and making moves on the game
+ * board.
+ *
+ * This class is a modified version of the code found at
+ * <a href="http://programmingbydoing.com/a/tic-tac-toe-oop.html">Original Project</a>
+ */
+public class TicTacToeGame {
+    /** The internal game board. */
+    private char[][] board;
+    /** The number of turns taken so far. */
+    private int turns;
+
+    /**
+     * Creates a new tic-tac-toe game instance and initializes the
+     * internal state of the game. After this method is called, the
+     * newly created game board is filled with '_' (empty spaces) to
+     * indicate that players have not yet occupied any spots.
+     */
+    public TicTacToeGame() {
+        board = new char[3][3];
+        turns = 0;
+
+        for (int r = 0; r < 3; r++ ) {
+            for (int c = 0; c < 3; c++ ) {
+                board[r][c] = ' ';
+            } // for
+        } // for
+    } // TicTacToeGame
+
+    /**
+     * Returns whether the specified character, {@code p}, has
+     * won the game.
+     *
+     * @return {@code true} if {@code p} has won the game and {@code false}
+     * otherwise.
+     * @param p the character to check.
+     */
+    public boolean isWinner( char p ) {
+        // TODO: implement me
+        throw new UnsupportedOperationException("isWinner is not implemented");
+    } // isWinner
+
+    /**
+     * Returns whether the game board is full.
+     *
+     * @return {@code true} if the board is full and {@code false} otherwise.
+     */
+    public boolean isFull() {
+        // TODO: implement me
+        throw new UnsupportedOperationException("isWinner is not implemented");
+    } // isFulle
+
+    /**
+     * Returns whether the game is a tie (cat game).
+     *
+     * @return {@code true} if the game is a tie and {@code false} otherwise.
+     */
+    public boolean isCat() {
+        // TODO: implement me
+        throw new UnsupportedOperationException("isWinner is not implemented");
+    } // isCat
+
+
+    /**
+     * Returns whether the specified coordinates, {@code r} and {@code c} fall
+     * within the boundaries of the game board.
+     *
+     * @param r the row number to check.
+     * @param c the column number to check.
+     * @return {@code true} if the specified coordinates are in bounds and
+     * {@code false} otherwise.
+     */
+    public boolean isValid( int r, int c ) {
+        if (0 <= r && r <= 2 && 0 <= c && c <= 2) {
+            return true;
+        } else {
+            return false;
+        } // if
+    } // isValie
+
+    /**
+     * Returns the number of turns taken so far in the game.
+     *
+     * @return the number of turns taken so far.
+     */
+    public int numTurns() {
+        return turns;
+    } // numTurns
+
+    /**
+     * Returns the player occupying the board location at the specified coordinates,
+     * {@code r} and {@code c}.
+     *
+     * @param r the row value to check.
+     * @param c the column value to check.
+     * @return the character representing the player located at position
+     * ({@code r},{@code c} on the board. If the location is blank, the method
+     * returns the {@code @} character.
+     */
+    public char playerAt( int r, int c ) {
+        if (isValid(r,c)) {
+            return board[r][c];
+        } else {
+            return '@';
+        } // if
+    } // playerAt
+
+    /**
+     * Prints the contents of the game board to the terminal.
+     */
+    public void displayBoard() {
+        System.out.println("  0  " + board[0][0] + "|" + board[0][1] + "|" + board[0][2]);
+        System.out.println("    --+-+--");
+        System.out.println("  1  " + board[1][0] + "|" + board[1][1] + "|" + board[1][2]);
+        System.out.println("    --+-+--");
+        System.out.println("  2  " + board[2][0] + "|" + board[2][1] + "|" + board[2][2]);
+        System.out.println("     0 1 2 ");
+    } // displayBoard
+
+    /**
+     * Makes a move for the specified player, {@code p}, at location
+     * ({@code r}, {@code c}) within the game board. If the move is invalid,
+     * the method has no effect on the underlying board. This method also
+     * increases the number of turns played.
+     *
+     * @param p the character representing the player making the move.
+     * @param r the row where the move is being made.
+     * @param c the column where the move is being made.
+     */
+    public void playMove( char p, int r, int c ) {
+        // TODO: implement me
+        throw new UnsupportedOperationException("isWinner is not implemented");
+    } // playMove
+
+} // TicTacToeGame
---
+++ src/TicTacToeRunner.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+/**
+ * {@code TicTacToeRunner} is the main entry point for a command-line Tic-Tac-Toe
+ * It relies on a working implementation of the {@code TicTacToeGame} class.
+ * The game is meant for two players ('X' and 'O') and each player takes turns
+ * trying to get three of their pieces in a row on the board. If a player gets
+ * three in a row, then they win. If the board fills up and no one gets three in a
+ * row, the game ends in a tie.
+ *
+ * This class is a modified version of the code found at
+ * <a href="http://programmingbydoing.com/a/tic-tac-toe-oop.html">Original Project</a>
+ */
+public class TicTacToeRunner {
+    /**
+     * The main entry point of the application.
+     *
+     * @param args command-line arguments to the program.
+     */
+	public static void main( String[] args ) {
+		Scanner keyboard = new Scanner(System.in);
+
+		char p = 'X';
+		TicTacToeGame ttt = new TicTacToeGame();
+		int r, c;
+
+        // the main game loop
+		while ( ! ( ttt.isWinner('X') || ttt.isWinner('O') || ttt.isFull() ) ) {
+			ttt.displayBoard();
+			System.out.print( "'" + p + "', choose your location (row, column): " );
+			r = keyboard.nextInt();
+			c = keyboard.nextInt();
+
+			while ( ttt.isValid(r,c) == false || ttt.playerAt(r,c) != ' ' ) {
+				if ( ttt.isValid(r,c) == false ) {
+					System.out.println("That is not a valid location. Try again.");
+                } else if ( ttt.playerAt(r,c) != ' ' ) {
+					System.out.println("That location is already full. Try again.");
+                } // if
+
+				System.out.print( "Choose your location (row, column): " );
+				r = keyboard.nextInt();
+				c = keyboard.nextInt();
+			}
+
+			ttt.playMove( p, r, c );
+
+			if ( p == 'X' ) {
+				p = 'O';
+            } else {
+				p = 'X';
+            } // if
+		} // while
+
+		ttt.displayBoard();
+
+		if ( ttt.isWinner('X') ) {
+			System.out.println("X is the winner!");
+        } // if
+		if ( ttt.isWinner('O') ) {
+			System.out.println("O is the winner!");
+        } // if
+		if ( ttt.isCat() ) {
+			System.out.println("The game is a tie.");
+        } // if
+	} // main
+}
